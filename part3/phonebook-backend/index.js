0678c9b87684b292bc93d667cbd2d30101230572
Ben <40966296+ben5315@users.noreply.github.com>
2025-05-04 13:38:18 +0100

    3c - MongoDB

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
+
 morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(express.static('dist'))
 
 let people = [
     {
@@ -31,24 +35,29 @@ let people = [
 ]
 
 app.get('/api/persons', (req, res) => {
-    res.json(people)
+    Person.find({}).then(people => {
+        res.json(people)
+    })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const person = people.find(p => p.id === id)
-
-    if (!person) {
-        res.status(404).end()
-    } else {
-        res.json(person)
-    }
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+    .then(person => {
+        if (person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    people = people.filter(p => p.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -58,26 +67,63 @@ app.post('/api/persons', (req, res) => {
         return res.status(400).json({error: 'Missing name'})
     } else if (!body.number) {
         return res.status(400).json({error: 'Missing number'})
-    } else if (people.some(p => p.name === body.name)) {
-        return res.status(400).json({error: `${body.name} already exists in the phonebook`})
     }
+    // } else if (people.some(p => p.name === body.name)) {
+    //     return res.status(400).json({error: `${body.name} already exists in the phonebook`})
+    // }
 
-    const person = {
-        // id: String(Math.max(...people.map(p => Number(p.id))) + 1),
-        id: String(Math.floor(Math.random()*1000)),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    people = people.concat(person)
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const { name, number } = req.body
+
+    Person.findById(req.params.id)
+    .then(person => {
+        if (!person) {
+            return res.status(404).end()
+        }
+
+        person.name = name
+        person.number = number
+
+        return person.save().then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
-    res.send(`<p>The phonebook has info for ${people.length} people.</p><br/><p>${Date()}</p>`)
+    Person.find({}).then(people => {
+        res.send(`<p>The phonebook has info for ${people.length} people.</p><br/><p>${Date()}</p>`)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'Unknown endpoint.'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (req, res, error, next) => {
+    if (error.name === "CastError") {
+        return res.status(400).send({ error: 'Malformed ID'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
